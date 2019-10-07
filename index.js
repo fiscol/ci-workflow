25b1887cc3ae317decbfccdf7e2e7d4d09c83162
fiscol <fiscol.wu@gmail.com>
2019-10-07 20:00:02 +0800

    191007_First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+let app = express();
+
+app.get('/', (req, res) =>{
+    res.send("Welcome board.");
+});
+
+const server = app.listen(3000, ()=>{
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log(`Example app listening at http://${host}:${port}`);
+});
